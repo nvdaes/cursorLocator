58e21654d99f70862d68844520948cae1cac1bd5
nvdaes <nrm1977@gmail.com>
2021-01-23 05:16:09 +0100

    Update template and GitHub Actions

--- buildVars.py
+++ buildVars.py
@@ -19,13 +19,19 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description" : _("""Reports cursor positions while typing on multiline edit controls."""),
 	# version
-	"addon_version" : "1.3-dev",
+	"addon_version" : ,
 	# Author(s)
 	"addon_author" : u"Noelia Ruiz Martínez <nrm1977@gmail.com>",
 	# URL for the add-on documentation support
-	"addon_url" : "https://addons.nvda-project.org",
+	"addon_url" : "https://github.com/nvdaes/cursorLocator",
 	# Documentation file name
 	"addon_docFileName" : "readme.html",
+	# Minimum NVDA version supported (e.g. "2018.3")
+	"addon_minimumNVDAVersion" : "2019.3",
+	# Last NVDA version supported/tested (e.g. "2018.4", ideally more recent than minimum version)
+	"addon_lastTestedNVDAVersion" : "2020.4",
+	# Add-on update channel (default is stable or None)
+	"addon_updateChannel" : None,
 }
 
 
